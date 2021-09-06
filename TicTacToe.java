e92520565b90f94a0b0d3320d05479318f6c2aa8
Shivani5078 <88040014+Shivani5078@users.noreply.github.com>
2021-09-06 11:36:26 +0530

    add:intial commit

---
+++ TicTacToe.java
@@ -0,0 +1,23 @@
+package  company;
+
+public class TicTacToe {
+	
+	static char board[];    // declaring an array
+	 public static void main(String[] args) {
+		 createBoard(); //method call
+		
+	}
+
+	//  Creating empty tic-tac-toe board
+	    public static void createBoard() {
+	        board = new char[10];   // creating an array
+
+	        
+	        for (int i = 1; i < board.length; i++) {
+	            board[i] = ' ';
+	        }
+	    }
+
+
+
+}
